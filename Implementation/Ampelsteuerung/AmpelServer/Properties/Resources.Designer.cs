dddbf3fc56c9277e9f676381712d81ff08c0609e
Christopher Wieland <cwieland.its-b2012@fh-salzburg.ac.at>
2017-04-28 07:59:02 +0200

    anfang zur KonfigDatei

--- Implementation/Ampelsteuerung/AmpelServer/Properties/Resources.Designer.cs
+++ Implementation/Ampelsteuerung/AmpelServer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SimpleServer.Properties {
+namespace Ampelsteuerung.Properties {
     using System;
     
     
