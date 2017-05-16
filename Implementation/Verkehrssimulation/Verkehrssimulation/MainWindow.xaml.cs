28bc111c21674357cc7c7a0112cb15446b223ec5
stephanie.kaschnitz <skaschnitz.its-m2016@fh-salzburg.ac.at>
2017-05-16 11:14:34 +0200

    RabbitMQ Kommunikation in die Main eingebunden - Klassen/Methoden gehören ausgelabert - Receive in eine While Schleife - Group2 / User + PW gehört noch angepasst -> da frag ich an Stefan

--- Implementation/Verkehrssimulation/Verkehrssimulation/MainWindow.xaml.cs
+++ Implementation/Verkehrssimulation/Verkehrssimulation/MainWindow.xaml.cs
@@ -7,6 +7,9 @@ using Verkehrssimulation.Verkehrsteilnehmer;
 using Ampelsteuerung;
 using CallbackCli;
 using System.ServiceModel;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Newtonsoft.Json;
 
 namespace Verkehrssimulation
 {
@@ -36,10 +39,154 @@ namespace Verkehrssimulation
             }
         }
 
+        /**
+         * RABBIT MQ
+         * 
+         * Group - User - PW gehört noch angepasst
+         * Methoden/Klassen gehören noch ausgelagert
+         * 
+         * */
+        static void Send(RemoteTransaction transaction)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = "amqp://user2:affe123456@rabbit.binna.eu/";  //Insert your own user and password
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var transactionString = JsonConvert.SerializeObject(transaction);   //Awesome function
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "car.production",  //Choose between bank.development and bank.production depending of the queue (e.g. 70 is production, 71 is development)
+                                    routingKey: "group2",   //This relates to the queue name of the receiver bank
+                                    basicProperties: properties,    //Set the properties to persistent, otherwise the messages will get lost if the server restarts
+                                    body: GetBytes(transactionString));
+
+            }
+
+        }
+
+        static void Receive()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = "amqp://user2:affe123456@rabbit.binna.eu/";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "group2",
+                                 durable: true, //Always use durable: true, because the queue on the server is configured this way. Otherwise you'll not be able to connect
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = GetString(ea.Body);
+                        var transaction = JsonConvert.DeserializeObject<RemoteTransaction>(body);
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false); //Important. When the message get's not acknowledged, it gets sent again
+
+                        Console.WriteLine("[x] Received:");
+                        PrintTransaction(transaction);
+                    };
+
+                    channel.BasicConsume(queue: "group2",
+                                         noAck: false,  //If noAck: false the command channel.BasicAck (see above) has to be implemented. Don't set it true, or the message will not get resubmitted, if the bank was offline
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit receive.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public class RemoteTransaction
+        {
+            public Guid CarId { get; set; }
+            public Guid GroupId { get; set; }
+            public String CarType { get; set; }
+            public double Speed { get; set; }
+            public long Timestamp { get; set; }        // Unix format timestamp in milliseconds
+            public int Errorcode { get; set; }         // 0 = no error
+
+            private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            public static long GetCurrentUnixTimestampMillis()
+            {
+                return (long)(DateTime.UtcNow - UnixEpoch).TotalMilliseconds;
+            }
+
+            public static DateTime DateTimeFromUnixTimestampMillis(long millis)
+            {
+                return UnixEpoch.AddMilliseconds(millis);
+            }
+
+            public RemoteTransaction(double speed, string cartype, int errorcode = 0)
+            {
+                this.CarId = Guid.NewGuid();
+                this.GroupId = Guid.NewGuid();
+                this.CarType = cartype;
+                this.Speed = speed;
+                this.Timestamp = GetCurrentUnixTimestampMillis();
+                this.Errorcode = errorcode;
+            }
+
+            public string GetStatus()
+            {
+                switch (this.Errorcode)
+                {
+                    case 0:
+                        return "Success";
+                    case -1:
+                        return "Json-Format invalid / Data could not be converted";
+                    case -2:
+                        return "Transaction rejected";
+                    default:
+                        return "Error not specified";
+                }
+            }
+        }
+
+        static void PrintTransaction(RemoteTransaction transaction)
+        {
+            Console.WriteLine("CarId    : " + transaction.CarId);
+            Console.WriteLine("GroupId  : " + transaction.GroupId);
+            Console.WriteLine("CarType : " + transaction.CarType);
+            Console.WriteLine("Speed    : " + transaction.Speed);
+            Console.WriteLine("Timestamp: " + transaction.Timestamp);
+        }
+
+        private static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             StartAmpelsteuerung();
+
+            var remoteTransaction = new RemoteTransaction(10.1, "PKW");
+
+      
+            Send(remoteTransaction);
+            Receive();
+            
 
             dispatchTimer = new DispatcherTimer();
             dispatchTimer.Tick += dispatchTimer_Tick;
