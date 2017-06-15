3fb5b159663959552f7a1626a395ecbfcb5cc68f
Christopher Wieland <cwieland.its-b2012@fh-salzburg.ac.at>
2017-06-15 10:19:04 +0200

    Ampelsteuerung interpreter redundantes entfernt

--- Implementation/Verkehrssimulation/Verkehrssimulation/Ampelsteuerung/CallbackClient.cs
+++ Implementation/Verkehrssimulation/Verkehrssimulation/Ampelsteuerung/CallbackClient.cs
@@ -1,26 +1,12 @@
 ﻿using Ampelsteuerung;
 using System;
-using Verkehrssimulation.AmpelHandler;
 using Verkehrssimulation;
 
 namespace CallbackCli
 {
     class CallbackClient: IAmpelCallback
     {
-
-        CallbackClient _client;
-        private AmpelHandler _client1;
         private MainWindow client;
-
-        public CallbackClient(AmpelHandler _client1)
-        {
-            this._client1 = _client1;
-        }
-
-        public CallbackClient(CallbackClient client)
-        {
-            _client = client;
-        }
 
         public CallbackClient()
         {
