c857f10257a36f1573c19b3cee3480b92f20bf2d
andli <andli@DESKTOP-DGA2IKU>
2017-06-18 15:35:46 +0200

    added from Hannes requested things

--- Implementation/Verkehrssimulation/Verkehrssimulation/Verkehrsteilnehmer/TrafficObject.cs
+++ Implementation/Verkehrssimulation/Verkehrssimulation/Verkehrsteilnehmer/TrafficObject.cs
@@ -16,7 +16,8 @@ namespace Verkehrssimulation.Verkehrsteilnehmer
         private int nextDirection;
         private Boolean mayDrive; //default false, should be set to false if car moved
         private int typ;
-       
+        private int waitingCounter; //if a car has not moved a certain time the system checks if it is caused by intersection deadlock and if there is a deadlock it chooses a random car that drives.
+
         public int Id { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
@@ -26,9 +27,11 @@ namespace Verkehrssimulation.Verkehrsteilnehmer
         public int Direction{ get; set; }
         public int NextDirection { get; set; }
         public Boolean MayDrive { get; set; }
+        public int Typ { get; set; }
         public int PassingObstacleStatus { get; set; }
+        public int WaitingCounter { get; set; }
 
-        public TrafficObject(int id, int x, int y, int speed, int direction, int nextDirection)
+    public TrafficObject(int id, int x, int y, int speed, int typ, int direction, int nextDirection)
         {
             this.Id = id;
             this.X = x;
@@ -38,6 +41,7 @@ namespace Verkehrssimulation.Verkehrsteilnehmer
             this.NextDirection = nextDirection;
             this.MayDrive = false;
             this.PassingObstacleStatus = (int) PassingObstStatus.RightSide;
+            this.WaitingCounter = 0;
         }
 
         public enum Dir{Up  =0, Left = 1, Down = 2, Right = 3 };// numbers are selceted that way so that are 90° ratation can be made by adding 1; !! do not change those, it will break the module!!
