34de561d3dbb78c5046009268bc9e3fb555fcbfa
andli <andli@DESKTOP-DGA2IKU>
2017-06-18 15:37:32 +0200

    added IGUI.cs

---
+++ Implementation/Verkehrssimulation/Verkehrssimulation/Verkehrsteilnehmer/IGUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verkehrssimulation.Verkehrsteilnehmer
+{
+    interface IGUI
+    {
+        void updateCarAmount(int cars);
+        void updateTruckRatio(int percent);
+    }
+}
