d235dfe90b62baeceea1adbe54ef83e47c6c9999
Wiesorium <wiesorium@gmail.com>
2017-06-26 17:49:48 +0200

    ampelanlagen ausschalten -> IF

--- Implementation/Verkehrssimulation/Verkehrssimulation/Verkehrsnetz/I_ENV_GUI.cs
+++ Implementation/Verkehrssimulation/Verkehrssimulation/Verkehrsnetz/I_ENV_GUI.cs
@@ -9,5 +9,6 @@ namespace Verkehrssimulation.Verkehrsnetz
     interface I_ENV_GUI
     {
         void addObstacle(int startx, int starty, int endx, int endy);
+        void setAmpelanlage(bool val);
     }
 }
